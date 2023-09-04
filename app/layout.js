5b94dfc8f1f93b17f79978ebc447148a0c3a1478
Vaaiibhav Vaidya <vaaiibhav@live.com>
2023-09-04 19:33:18 +0530

    login page

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import SidebarWithSearch from "@/Components/SidebarWithSearch";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import Login from "@/Components/Login";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Magic X Games",
+  title: "Blue Fighter Admin Panel",
   description: "Developed by Vaaiibhav",
 };
 
@@ -13,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SidebarWithSearch user="Vaaiibhav" /> {children}
+        {/* <SidebarWithSearch user="Vaaiibhav" /> {children} */}
+
+        {<Login />}
       </body>
     </html>
   );
