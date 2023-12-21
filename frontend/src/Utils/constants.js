517254752f71a70d55bdc9305a3440f783322d82
Vaaiibhav Vaidya <vaaiibhav@live.com>
2023-12-21 14:19:50 +0530

    events complete till winning Number

--- frontend/src/Utils/constants.js
+++ frontend/src/Utils/constants.js
@@ -2,4 +2,5 @@ const LOCAL_IP = "http://192.168.1.2:8000/";
 const SERVER_IP = "http://16.163.126.173:8000/";
 const LOCALHOST = "http://localhost:8000/";
 const BACKEND_URI = LOCAL_IP;
+const gudGudiToken = "gudGudiAdminToken";
 module.exports = { BACKEND_URI };
